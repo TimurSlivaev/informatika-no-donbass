4a74ea368ceeb6d19fe4e36cfeee58d1d5c89af6
TimurSlivaev <66788988+TimurSlivaev@users.noreply.github.com>
2022-02-20 20:13:40 +0300

    /

---
+++ сюда/задание 17/kak rabot.py
@@ -0,0 +1,19 @@
+f = open('задание 17\kak rabot.txt')
+
+n1=f.readline()
+n2=f.readlines()
+
+print(n1) 
+print(n2)
+#2
+
+#['13\n', '15\n', '12\n', '53']
+
+"""
+но при этом!
+n2=f.readlines()
+print(n2)
+
+#['2\n', '13\n', '15\n', '12\n', '53']
+
+"""
